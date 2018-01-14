6788cc1e72b87699d5cb09781a00e9ff37551c61
sin <xuanmingyi@gmail.com>
2018-01-15 04:24:06 +0800

    Modify run.sh to better

--- build/run.sh
+++ build/run.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 HERE=`pwd`
-FALCON=$GOPATH/src/github.com/open-falcon/falcon-plus
+FALCON=$HERE/falcon-plus
+
+if [ -n $GOPATH ];then
+    FALCON=$GOPATH/src/github.com/open-falcon/falcon-plus
+fi
+
+if [ ! -d $FALCON ];then
+    git clone https://github.com/open-falcon/falcon-plus.git $FALCON
+fi
+
+which docker
+if [ $? != 0 ];then
+    echo "need install docker"
+    exit
+fi
+
 sudo docker pull golang
 
 cd $FALCON;git pull
